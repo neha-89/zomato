2a5f0762d17d1aeca551fae11277e1fb7291011c
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-05 22:43:56 +0530

    added the component

---
+++ zomapp/src/components/Home/QuickSearchDisplay.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Component } from 'react';
+
+class QuickSearchDisplay extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default QuickSearchDisplay;
