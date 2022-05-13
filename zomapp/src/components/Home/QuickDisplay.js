17794704d4d2947182d1fcb39cb50e4ce9242d20
Neha Sontakke <wasnik.neha@gmail.com>
2022-05-13 18:37:55 +0530

    added the quicksearch api

--- zomapp/src/components/Home/QuickDisplay.js
+++ zomapp/src/components/Home/QuickDisplay.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Component } from 'react';
+import {Link} from 'react-router-dom';
 
-class QuickSearchDisplay extends Component{
-    constructor(){
-        super()
+const QuickDisplay = (props)=>{
+    const listMeal = ({mealData})=>{
+        if(mealData){
+            return mealData.map((item)=>{
+                return(
+                    <>
+           <Link to={`/listing/${item.mealtype_id}`} key={item._id}>         
+            
+                
+                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+                        <div className="card">
+                        <img src={item.meal_image} alt={item.mealtype} class="card-img-top"/>
+                            
+                            <div className="card-body">
+                                
+                                 <p className ="card-text">{item.mealtype}</p>
+                              
+                                <p className="card-text">{item.content}</p>
+                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
+                            </div>
+                        </div>
+                    </div>
+               
+    
+        </Link>
+                    </>
+                )
+            })
+        }
     }
-    render(){
-        return(
-            <>
-            </>
-        )
-    }
+    return(
+        <>
+        <div id="container">
+          <div className = "row">
+          {listMeal(props)}
+          </div>
+        </div>
+        </>
+    )
+    
 }
 
-export default QuickSearchDisplay;
+export default QuickDisplay;
+
--- zomapp/src/components/Home/QuickSearch.js
+++ zomapp/src/components/Home/QuickSearch.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Component } from 'react';
+import QuickDisplay from './QuickDisplay';
 
+let url ="https://zomatoajulypi.herokuapp.com/quicksearch";
 
 class QuickSearch extends Component {
     constructor(){
         super()
+        this.state = {
+            mealType :''
+        }
     }
     render(){
         return(
@@ -16,73 +21,18 @@ class QuickSearch extends Component {
             <span id="QuickSubHeading">
                 Discover Restaurants By Meal
             </span>
-            <div className="container">
-                <div className="row">
-                    <div className="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div className="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div className="card-body">
-                                <h5 className="card-title">Breakfast</h5>
-                                <h6 className="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div class="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Breakfast</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div class="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Breakfast</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div class="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Breakfast</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div class="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Breakfast</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div> 
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
-                        <div class="card">
-                            <img src="../images/sacks.png" alt="snacks"class="card-img-top"/>
-                            <div class="card-body">
-                                <h5 class="card-title">Breakfast</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">Enjoy Your BreakFast Meal</h6>
-                              {/* <!-- <a href="#" class="btn btn-primary">Go somewhere</a> --> */}
-                            </div>
-                        </div> 
-                    </div>
-                </div>
-            </div>
+            <QuickDisplay mealData ={this.state.mealType} />
+            
          </div> 
             </>
         )
+    }
+    componentDidMount(){
+        fetch(url,{method:'GET'})
+        .then((res) => res.json())
+        .then((data) => {
+            this.setState({mealType:data})
+        })
     }
 }
 
